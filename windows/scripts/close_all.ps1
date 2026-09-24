2fd8189cb13c3fccafa392430ef7cdbad863dd12
jbwv <jamesbaldwinwv@gmail.com>
2026-09-24 08:05:08 -0400

    Delete windows directory

--- windows/scripts/close_all.ps1
+++
@@ -1,3 +0,0 @@
-(New-Object -ComObject Shell.Application).Windows() | ForEach-Object { $_.Quit() }
-Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | ForEach-Object { $_.CloseMainWindow() }
-Get-Process powershell -ErrorAction SilentlyContinue | Stop-Process -Force
